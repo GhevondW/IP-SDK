824e9d20a36e50883069d2562e21f544bcc9f92d
Ghevond <ghevond.98@gmail.com>
2020-03-08 12:39:11 +0400

    opencv included

--- IP-SDK/IP-SDK/Main.cpp
+++ IP-SDK/IP-SDK/Main.cpp
@@ -1,10 +1,12 @@
 #include "GlobalDefines.h"
 #include "Image.h"
+#include <opencv2/opencv.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+
 
 using namespace sdk;
 
@@ -26,6 +28,7 @@ int main()
 	free(a);
 	*/
 
+	/*
 	int iWidth, iHeight, iChannels;
 	unsigned char* img = stbi_load("fbs1477_cor.tiff", &iWidth, &iHeight, &iChannels, 0);
 
@@ -34,6 +37,12 @@ int main()
 	    exit(1);
 	}
 	printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", iWidth, iHeight, iChannels);
+	*/
 
+	cv::Mat img = cv::imread("test.jpg");
+
+	cv::namedWindow("image", cv::WINDOW_NORMAL);
+	cv::imshow("image", img);
+	cv::waitKey(0);
 	return 0;
 }
