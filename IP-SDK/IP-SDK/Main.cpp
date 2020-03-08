324140178a56858afecebcd1cf66040d4c5a12a0
Ghevond <ghevond.98@gmail.com>
2020-03-08 11:14:32 +0400

    jpeg and png image reding lib added

--- IP-SDK/IP-SDK/Main.cpp
+++ IP-SDK/IP-SDK/Main.cpp
@@ -1,10 +1,16 @@
 #include "GlobalDefines.h"
 #include "Image.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 using namespace sdk;
 
 int main() 
 {
+	/*
 	int* a = (int*)malloc(20*sizeof(int));
 	Image* img = Image::Make()
 		.SetBpp(2)
@@ -18,6 +24,16 @@ int main()
 
 	delete img;
 	free(a);
+	*/
+
+	int iWidth, iHeight, iChannels;
+	unsigned char* img = stbi_load("fbs1477_cor.tiff", &iWidth, &iHeight, &iChannels, 0);
+
+	if (img == NULL) {
+		printf("Error in loading the image\n");
+	    exit(1);
+	}
+	printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", iWidth, iHeight, iChannels);
 
 	return 0;
 }
